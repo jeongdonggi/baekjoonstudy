17928f45ef0ca270668bb84d9210697a84d67b5a
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2024-05-01 22:19:05 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,9 @@
+def solution(date1, date2):
+    answer = 0
+    for i in range(3):
+        if date1[i] < date2[i]:
+            return 1
+        elif date1[i] > date2[i]:
+            break
+            
+    return answer
