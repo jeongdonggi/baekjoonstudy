122c2584ba679f3c4a5c9b086d4055c8bf8d8a04
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2024-05-01 21:40:16 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,6 +1,4 @@
 def solution(seoul):
     answer = ''
-    for str in seoul:
-        if str == "Kim":
-            answer = (f"김서방은 {seoul.index(str)}에 있다")
+    answer = f"김서방은 {seoul.index('Kim')}에 있다"
     return answer
