89031110155447013ce3c72eb8b71e47182e8c1c
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2024-05-29 14:12:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = True
+    if len(s) == 4 or len(s) == 6:
+        if not s.isdigit():
+            return False
+    else:
+        return False
+            
+    return answer
