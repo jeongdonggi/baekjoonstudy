d68939d0a943039e135648c387ad2fbdd397442d
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2024-05-29 18:44:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -2,18 +2,10 @@ def solution(A,B):
     answer = 0
 
     a1 = sorted(A)
-    b1 = sorted(B)
-    
-    a2 = a1[::-1]
-    b2 = b1[::-1]
-    
-    re1 = 0
-    re2 = 0
+    b2 = sorted(B)[::-1]
     
     for i in range(len(a1)):
-        re1 += a1[i]*b2[i]
-        re2 += a2[i]*b1[i]
-        
-    answer = min(re1, re2)
+        answer += a1[i]*b2[i]
+
         
     return answer
