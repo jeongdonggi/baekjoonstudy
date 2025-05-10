6ec8f9095153a1676bc761a09dd44c853ba79852
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2025-05-10 17:18:37 +0900

    [Silver IV] Title: 동전 0, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,23 +1,17 @@
-"""
-N 종류의 돈으로 K를 만드려고 함 => 최솟값
-"""
-
 n, k = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+arr.sort(reverse=True)
 
 result = 0
 
-li = []
-for i in range(n):
-    m = int(input())
-    li.append(m)
-
-li = li[::-1]
-
-for money in li:
-    if k < money:
+for j in arr:
+    if k < j:
         continue
-    else:
-        result += k // money
-        k = k % money
+
+    result += k // j
+    k = k % j
 
 print(result)
