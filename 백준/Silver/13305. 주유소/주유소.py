c6d8095a8c9ac4a373163f999f93615e7739ccd6
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2025-05-14 21:06:30 +0900

    [Silver III] Title: 주유소, Time: 140 ms, Memory: 137512 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,16 +2,15 @@ n = int(input())
 load = list(map(int, input().split()))
 city = list(map(int, input().split()))[:-1]
 
-maximun = max(city)
-cnt = sum(load)
+min = city[0]
 
-result = cnt * maximun
+result = 0
 
 for i in range(n-1):
-    if city[i] < maximun:
-        result -= (max(city) - city[i]) * sum(load[i:])
-        maximun = city[i]
+    if city[i] < min:
+        result += city[i] * load[i]
+        min = city[i]
     else:
-        continue
+        result += min * load[i]
 
 print(result)
