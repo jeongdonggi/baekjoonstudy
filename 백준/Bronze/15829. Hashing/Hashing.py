f38ae5ed7009f994cb1b726f025aff9930299ad8
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2025-06-09 18:16:12 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -5,8 +5,10 @@ l = int(input())
 s = input()
 
 result = 0
+power = 1
 
 for i in range(len(s)):
-    result += (ord(s[i]) - 96) * (r**i)
+    result = (result + (ord(s[i]) - 96) * power) % m
+    power = power * r  # 지수 대신 곱하기 사용
 
 print(result)
