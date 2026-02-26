64eff48e4fe47122f7e050eefc1c7f4f462f63a4
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2026-02-26 22:30:16 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int n) {
-        String[] answer = Arrays.copyOf(my_string.split(""), n);
-        return String.join("", answer);
+        return my_string.substring(0, n);
     }
 }
