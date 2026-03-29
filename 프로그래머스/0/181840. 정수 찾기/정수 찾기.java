ffe6008236b9ee1c327640c7824d19c227f3c93f
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2026-03-29 18:58:41 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,11 +1,7 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] num_list, int n) {
         int answer = 0;
-        
-        Arrays.sort(num_list);
-        
+                
         for(int num : num_list) {
             if (num == n) {
                 answer = 1;
