d67bd0651142c4d3d5722d88bca0d7de45889e4d
jeongdonggi <100845304+jeongdonggi@users.noreply.github.com>
2026-05-18 21:49:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,11 +1,16 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        if (common[1] - common[0] == common[common.length -1] - common[common.length - 2]) {
-            answer = common[common.length - 1] + common[1] - common[0];
+        
+        int first = common[1] - common[0];
+        int second = common[common.length -1] - common[common.length -2];
+        
+        if (second - first == 0) {
+            answer = common[common.length -1 ] + second;
         } else {
-            answer = common[common.length -1] * (common[1] / common[0]);
+            answer = common[common.length -1 ] * (common[1] / common[0]);
         }
+        
         return answer;
     }
 }
